2e8b1c0e250fb6a5971dd4fb455535aaee0c1100
Jamie McCrocklin <jamiemccrocklin@gmail.com>
2019-06-12 11:18:33 -0400

    Added dropdown menu

--- index.js
+++ index.js
@@ -18,4 +18,21 @@ $(document).ready(function(){
     })
 })
 
-//when hovering over project image, message pops up to "view project"
+
+//on click of hamburger, dropdown menu with li options
+$(document).ready(function(){
+    $(document).on('click', '.menu-btn', function(){
+        $('.navItems').slideToggle(500);
+    });
+})
+
+//Dropdown menu closes after clicking a link 
+
+
+
+
+//dropdown menu closes on click anywhere outside of the menu
+
+$(document).click(function(){
+    $('.navItems').hide();
+});
